b3bb6b356a2bcf924b859e0d7f3646cd0f2ed4c6
WorldHim <worldhim@hotmail.com>
2026-09-15 02:05:38 +0800

    bump version v1.1.0

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ except ImportError:  # 旧版 AstrBot 无该模块时回退为占位符记录
     ReplyChainParser = None
 
 # 插件版本(@register 与 metadata.yaml 的 version 保持一致)
-PLUGIN_VERSION = "1.0.0"
+PLUGIN_VERSION = "1.1.0"
 
 # 插件唯一识别名(数据目录 data/plugin_data/{PLUGIN_NAME},与 metadata.yaml 的 name 保持一致)
 PLUGIN_NAME = "astrbot_plugin_archiver"
